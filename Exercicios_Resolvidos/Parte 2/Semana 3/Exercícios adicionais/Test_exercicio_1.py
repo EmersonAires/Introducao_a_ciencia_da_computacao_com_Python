4084eb0ae05711195a9a42523abe22a3cb7b98bf
Emerson Eduardo Aires Nunes <emersoneduardo.airesnunes@gmail.com>
2021-02-04 15:27:27 -0300

    teste do método semelhantes acrescentado

--- Exercicios_Resolvidos/Parte 2/Semana 3/Exercícios adicionais/Test_exercicio_1.py
+++ Exercicios_Resolvidos/Parte 2/Semana 3/Exercícios adicionais/Test_exercicio_1.py
@@ -9,3 +9,14 @@ class Test_exercio_1:
 
         t1 = exercicio1_triangulos_retangulos.Triangulo(a, b, c)
         assert t1.retangulo() == valor_esperado
+    
+    @pytest.fixture
+    def t1(self):
+        t1 = exercicio1_triangulos_retangulos.Triangulo(2, 2, 2)
+        return t1
+
+    @pytest.mark.parametrize("a, b, c, valor_esperado", [(1, 2, 3, False),(3,2, 5, False), (8, 8, 8, True), (4, 4, 4, True)])
+
+    def test_triangulos_semelhantes(self, t1, a, b, c, valor_esperado):
+        t2 = exercicio1_triangulos_retangulos.Triangulo(a, b, c)
+        assert t1.semelhantes(t2) == valor_esperado
