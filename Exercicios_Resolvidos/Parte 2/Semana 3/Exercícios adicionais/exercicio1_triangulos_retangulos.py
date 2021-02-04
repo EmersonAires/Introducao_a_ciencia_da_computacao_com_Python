50773ffa96b2190a5b77c65176d6684c7f95c19c
Emerson Eduardo Aires Nunes <emersoneduardo.airesnunes@gmail.com>
2021-02-04 15:26:48 -0300

    método semelhantes do exercício 2 - resolvido

--- Exercicios_Resolvidos/Parte 2/Semana 3/Exercícios adicionais/exercicio1_triangulos_retangulos.py
+++ Exercicios_Resolvidos/Parte 2/Semana 3/Exercícios adicionais/exercicio1_triangulos_retangulos.py
@@ -46,3 +46,21 @@ class Triangulo:
                 return True
             else:
                 return False
+    
+    def semelhantes(self, triangulo):
+
+        l1 = [self.a, self.b, self.c]
+        l2 = [triangulo.a, triangulo.b, triangulo.c]
+        l1.sort()
+        l2.sort()
+        
+        if ((l1[0]/l2[0]) == (l1[1]/l2[1]) == (l1[2]/l2[2])):
+           return True
+        else:
+            return False
+
+
+
+
+    
+
