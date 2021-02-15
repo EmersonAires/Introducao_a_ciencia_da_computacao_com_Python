73cdaf6295233d943349519e6da09d11f9b5b9a3
Emerson Eduardo Aires Nunes <emersoneduardo.airesnunes@gmail.com>
2021-02-14 22:15:27 -0300

    Algoritmos de inserção e seleção implementados

--- Exercicios_Resolvidos/Parte 2/Semana 5/teste/ordena.py
+++ Exercicios_Resolvidos/Parte 2/Semana 5/teste/ordena.py
@@ -29,3 +29,36 @@ class ordenacao:
                 return list
 
         return list
+
+    def insercao(self, list):
+
+
+        for i in range(1, len(list)):
+            element = list[i] # Guarda o valor do elemento analisado
+            j = i
+            while j > 0 and list[j-1] > element:
+                list[j] = list[j-1]  # Desloca o número para a direita 
+                j -= 1
+            list[j] = element # Inseri o elemento na posição adequada
+        
+        return list
+    
+    def selecao(self, list):
+
+
+        for i in range(len(list)-1):
+            min = i       # considera o prímeiro elemento como mínimo inicial
+
+            for j in range(i+1, len(list)):
+                if list[j] < list[min]:
+                    min = j             # nova posição do mínimo
+
+            if min != i:
+                list[min], list[i] = list[i], list[min] # Inverte as posições
+        
+        return list
+
+ord = ordenacao()
+list = [5, 4, 3, 2, 1]
+
+ord.selecao(list)
