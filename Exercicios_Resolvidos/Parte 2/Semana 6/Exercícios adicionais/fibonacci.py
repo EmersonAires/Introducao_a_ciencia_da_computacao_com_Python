c31a372c9065aac11c409c7c7dedc1261ee3debb
Emerson Eduardo Aires Nunes <emersoneduardo.airesnunes@gmail.com>
2021-09-19 09:52:12 -0300

    add docstring and comments

--- Exercicios_Resolvidos/Parte 2/Semana 6/Exercícios adicionais/fibonacci.py
+++ Exercicios_Resolvidos/Parte 2/Semana 6/Exercícios adicionais/fibonacci.py
@@ -1,6 +1,11 @@
 def fibonacci(n):
-    
-    if n <= 1:
+    '''
+    Returns the n term in the fibo sequence [1, 1, 2, 3, 5, 8...]
+    '''  
+    if n <= 1: # Base case
         return n
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2) # Recursive case
+
+
+print(fibonacci(5))
